a0784d370c0f92cd45f5b98050d30d1f6791f8ad
An-Tonic <aserdyukov@uclan.ac.uk>
2022-11-16 23:38:39 +0200

    Products page: item page dynamic

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-items_array = [['UCLan Hoodie','Purple','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (1).jpg'],
+let items_array = [
+    ['','hoodie','','',''],
+    ['UCLan Hoodie','Purple','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (1).jpg'],
     ['UCLan Hoodie','Light Blue','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (2).jpg'],
     ['UCLan Hoodie','Green','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (3).jpg'],
     ['UCLan Hoodie','Dark Grey','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (4).jpg'],
@@ -32,7 +34,7 @@ items_array = [['UCLan Hoodie','Purple','cotton authentic character and practica
     ,['UCLan Hoodie','Burgundy New','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (32).jpg']
     ,['UCLan Hoodie','Navy New','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (33).jpg']
     ,['UCLan Hoodie','Bright Green','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (34).jpg']
-    ,['','','','','']
+    ,['','jumper','','','']
     ,['UCLan Logo Jumper','Purple','cotton authentic character and practicality are combined in this winter jumper for students that goes with everything to create casual looks',' Only Ј29.99','images/items/jumpers/jumper (1).jpg']
     ,['UCLan Logo Jumper','Rusty Red','cotton authentic character and practicality are combined in this winter jumper for students that goes with everything to create casual looks',' Only Ј29.99','images/items/jumpers/jumper (2).jpg']
     ,['UCLan Logo Jumper','Water Blue','cotton authentic character and practicality are combined in this winter jumper for students that goes with everything to create casual looks',' Only Ј29.99','images/items/jumpers/jumper (3).jpg']
@@ -73,7 +75,7 @@ items_array = [['UCLan Hoodie','Purple','cotton authentic character and practica
     ,['UCLan Logo Jumper','Creame','cotton authentic character and practicality are combined in this winter jumper for students that goes with everything to create casual looks',' Only Ј29.99','images/items/jumpers/jumper (38).jpg']
     ,['UCLan Logo Jumper','Lighter Blue','cotton authentic character and practicality are combined in this winter jumper for students that goes with everything to create casual looks',' Only Ј29.99','images/items/jumpers/jumper (39).jpg']
     ,['UCLan Logo Jumper','Light Grey','cotton authentic character and practicality are combined in this winter jumper for students that goes with everything to create casual looks',' Only Ј29.99','images/items/jumpers/jumper (40).jpg']
-    ,['','','','','']
+    ,['','tshirt','','','']
     ,['UCLan Logo Tshirt','Navy Blue New','cotton authentic character and practicality are combined in this summery t-shirt for students that goes with everything to create casual looks. Perfect for those summer days',' Only Ј19.99','images/items/tshirt/tshirt (1).jpg']
     ,['UCLan Logo Tshirt','Rusty Red New','cotton authentic character and practicality are combined in this summery t-shirt for students that goes with everything to create casual looks. Perfect for those summer days',' Only Ј19.99','images/items/tshirt/tshirt (2).jpg']
     ,['UCLan Logo Tshirt','Burgundy','cotton authentic character and practicality are combined in this summery t-shirt for students that goes with everything to create casual looks. Perfect for those summer days',' Only Ј19.99','images/items/tshirt/tshirt (3).jpg']
@@ -115,27 +117,51 @@ items_array = [['UCLan Hoodie','Purple','cotton authentic character and practica
 function add_items(array){
 
     for (let i = 0; i < array.length; i++){
-        if (array[i][0] !== ''){
+        //Adding jump tags that are stored in an empty array, exept for second element that stores the id name
+        if (array[i][0] === '') {
             let item = document.createElement("div");
-            item.className = "item";
-            item.innerHTML = "<img class=\"item_img\" src=\"coursework/assignment%201%20resources/" + array[i][4] + "\" alt=\"\">\n" +
-                "                <div class=\"item_details\">\n" +
-                "                    <h1 class=\"item_name\">" + array[i][0] + " - " + array[i][1] + "</h1>\n" +
-                "                    <span class=\"item_description\">" + array[i][2] + "</span>\n" +
-                "\n" +
-                "                    <span class=\"item_price\">" + array[i][3] + "</span>\n" +
-                "\n" +
-                "                    <span class=\"item_buy_button\">Add to cart</span>\n" +
-                "\n" +
-                "                </div>";
-
+            item.setAttribute("id", array[i][1]);
             document.getElementById("products").appendChild(item);
-            console.log("Item appended");
+        }else {
+            //Create an item box with pictures etc.
+            item_to_html(array[i], i);
         }
-
     }
 }
 
-add_items(items_array)
+function item_to_html(array, i){
+    let item = document.createElement("div");
+    item.className = "item";
+    let str = "sf"
+    item.innerHTML = "<a href=\"item.html\" onclick=\"load_item(" + i + ")\"><img class=\"item_img\" src=\"coursework/assignment%201%20resources/" + array[4] + "\" alt=\"\"></a>\n" +
+        "                 <div class=\"item_details\">\n" +
+        "                     <h1 class=\"item_name\">" + array[0] + " - " + array[1] + "</h1>\n" +
+        "                     <span class=\"item_description\">" + array[2] + "</span>\n" +
+        "\n" +
+        "                     <span class=\"item_price\">" + array[3] + "</span>\n" +
+        "\n" +
+        "                     <button class=\"item_buy_button\" onclick=\"add_to_cart()\">Add to cart</button>" +
+        "\n" +
+        "                </div>";
+
+    document.getElementById("products").appendChild(item);
+}
+
+function add_to_cart(){
+    alert("The item was added to the cart");
+    localStorage.setItem("item","information");
+}
+
+function load_item(item_index){
+
+    sessionStorage.setItem("item", item_index);
+}
+
+function item_to_html2(){
+    //TODO Pass name
+    let index = sessionStorage.getItem("item")
+    item_to_html(items_array[index], index)
+}
+
 
 
