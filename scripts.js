f7a48a42556cc00c2f170a8a5d407eb109edd5e9
an-tonic <aserdyukov@uclan.ac.uk>
2022-12-16 14:15:55 +0200

    Cart CSS, and cart working functionality

--- scripts.js
+++ scripts.js
@@ -7,45 +7,71 @@ function display_items(array){
             let item = document.createElement("div");
             item.setAttribute("id", array[i][1]);
             document.getElementById("products").appendChild(item);
-        }else {
+        } else {
             // Create an item box with pictures etc.
             item_to_html(array[i], i);
         }
     }
+
 }
 
-function add_items_to_cart(js_obj, i){
-    let counter = 0;
-    for (let key in (js_obj)){
-        //A localSession object has unneeded keys which are filtered out
-        if (js_obj.hasOwnProperty(key)){
-            counter++;
+function display_items_in_cart(){
+    // let counter = 0;
+    // for (let key in (js_obj)){
+    //     //A localSession object has unneeded keys which are filtered out
+    //     if (js_obj.hasOwnProperty(key)){
+    //         counter++;
+    //
+    //         let array = js_obj[key].split(',');
+    //         let row  = document.createElement("tr");
+    //
+    //         row.innerHTML =
+    //             "           <td>" + counter + "</td>\n" +
+    //             "           <td><a href='item.html' onclick=\"load_item('" + array + "', '" + i + "')\">" +
+    //             "                    <img class='item_img' src='coursework/assignment_1_resources/" + array[4] + "' alt='" + array[0] + "'>" +
+    //             "                 </a></td>\n" +
+    //             "           <td>" + array[0] + "</td>\n" +
+    //             //The "slice" will leave only the price
+    //             "           <td>" + array[3].slice(5) + "</td>" +
+    //             "<div class=\"items_quantity_button\" > <button onclick=\"change_quantity('" + counter + "', '" + i + "', '" + 1 + "')\">+</button> <div id=\"item_quantity" + counter + "\" >1</div><button onclick=\"change_quantity('" + counter + "', '" + i + "', '" + -1 + "')\">-</button></div>";
+    //
+    //         document.getElementById("table_body").appendChild(row);
+    //
+    //     }
+    // }
 
-            let array = js_obj[key].split(',');
-            let row  = document.createElement("tr");
+    let keys = Object.keys(localStorage);
 
-            row.innerHTML =
-                "           <td>" + counter + "</td>\n" +
-                "           <td><a href='item.html' onclick=\"load_item('" + array + "', '" + i + "')\">" +
-                "                    <img class='item_img' src='coursework/assignment_1_resources/" + array[4] + "' alt='" + array[0] + "'>" +
-                "                 </a></td>\n" +
-                "           <td>" + array[0] + "</td>\n" +
-                //The "slice" will leave only the price
-                "           <td>" + array[3].slice(5) + "</td>" +
-                "<div class=\"items_quantity_button\" > <button onclick=\"change_quantity('" + counter + "', '" + i + "', '" + 1 + "')\">+</button> <div id=\"item_quantity" + counter + "\" >1</div><button onclick=\"change_quantity('" + counter + "', '" + i + "', '" + -1 + "')\">-</button></div>";
+    for (let i = 0; i < keys.length; i++){
+        let array = localStorage.getItem(keys[i]).split(',');
+        let row  = document.createElement("tr");
+        row.setAttribute('id', keys[i]);
+        row.innerHTML =
+                    "           <td>" + (i+1) + "</td>\n" +
+                    "           <td><a href='item.html' onclick=\"load_item('" + array + "', '" + keys[i] + "')\">" +
+                    "                    <img class='item_img' src='coursework/assignment_1_resources/" + array[4] + "' alt='" + array[0] + "'>" +
+                    "                 </a>" +
+                    "           </td>" +
+                    "           <td>" + array[0] + "</td>" +
+                    //The "slice" will leave only the price
+                    "           <td>" + array[3].slice(5) + "</td>" +
+                    "<div class=\"items_quantity_button\" >" +
+                    "   <button onclick=\"add_quantity('" + i + "')\">+</button> " +
+                    "       <div id=\"item_quantity" + i + "\" >1</div>" +
+                    "   <button onclick=\"decrease_quantity('" + i + "', '" + keys[i] + "')\">-</button>" +
+                    "</div>";
 
-            document.getElementById("table_body").appendChild(row);
-
-        }
+                document.getElementById("table_body").appendChild(row);
     }
+
 }
 
 function item_to_html(array, itemID){
+
     let button_text = "Add to cart";
     if ( localStorage.getItem("itemID" + itemID) !== null){
         button_text = "✔";
     }
-
 
     let item = document.createElement("div");
     item.className = "item";
@@ -84,34 +110,36 @@ function load_item(array, itemID){
     sessionStorage.setItem('ID', itemID);
 }
 
-
-
 function clear_cart(){
-    let clear = false;
 
     if (localStorage.length > 0){
-        clear = confirm("Are you sure you want to clear the cart?");
+        let clear = confirm("Are you sure you want to clear the cart?");
+        if (clear){
+            localStorage.clear()
+        }
     } else {
         alert("The cart is already empty!")
     }
 
-    if (clear){
-        localStorage.clear()
-    }
+
 }
 
-function change_quantity(row_ID, itemID, n){
+function add_quantity(row_ID){
     let increase_button = document.getElementById("item_quantity" + row_ID);
-    if (parseInt(increase_button.innerText) > 1){
-        increase_button.innerText = (parseInt(increase_button.innerText) + parseInt(n)).toString();
-    } else {
+    increase_button.innerText = (parseInt(increase_button.innerText) + 1).toString();
+}
+
+function decrease_quantity(row_ID, itemID){
+    let increase_button = document.getElementById("item_quantity" + row_ID);
+    if(increase_button.innerText === "1"){
         let delete_item = confirm("Are you sure you want to delete this item from the cart?")
         if (delete_item){
-            console.log("itemID" + itemID)
-            localStorage.removeItem("itemID" + itemID);
-        }
-    }
 
+            localStorage.removeItem(itemID);
+            document.getElementById(itemID).innerHTML = "";
+        }
+
+    }
+    increase_button.innerText = (parseInt(increase_button.innerText) - 1).toString();
 
 }
-
